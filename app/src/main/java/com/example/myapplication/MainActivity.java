c889e6049727c625e04b41988761bee899a34b7f
Gautham Velupally <gvelu0326@gmail.com>
2023-09-08 11:58:33 -0500

    Adding Functionality: Button goes somewhere
    Button now leads to the takeInput function where the numbers are set up. Need to set up the operation buttons next: reset currentNum, add the currentNum to NumArr, add the operation to OperationsArr.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,62 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    String currentNum = "";
+    String view = "";
+    ArrayList<Integer> NumArr;
+    ArrayList<String> OperationsArr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //view v is the button object?
+    public void takeInput(View v){
+        //integers
+        if (v.getId() == R.id.button0){
+            currentNum = currentNum + "0";
+        }
+        else if (v.getId() == R.id.button1){
+            currentNum = currentNum + "1";
+        }
+        else if (v.getId() == R.id.button2){
+            currentNum = currentNum + "2";
+        }
+        else if (v.getId() == R.id.button3){
+            currentNum = currentNum + "3";
+        }
+        else if (v.getId() == R.id.button4){
+            currentNum = currentNum + "4";
+        }
+        else if (v.getId() == R.id.button5){
+            currentNum = currentNum + "5";
+        }
+        else if (v.getId() == R.id.button6){
+            currentNum = currentNum + "6";
+        }
+        else if (v.getId() == R.id.button7){
+            currentNum = currentNum + "7";
+        }
+        else if (v.getId() == R.id.button8){
+            currentNum = currentNum + "8";
+        }
+        else if (v.getId() == R.id.button9){
+            currentNum = currentNum + "9";
+        }
+        //operations
+        else if (v.getId() == R.id.buttonAdd){
+            currentNum = currentNum + "9";
+        }
     }
 }
